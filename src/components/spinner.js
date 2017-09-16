d9f43764b1249583eeac095c63ae669989831eb1
Dmytro Lvivsky <dmytro.lvivsky@gmail.com>
2017-09-16 12:04:16 +0200

    Fixed spinner allignment

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -3,9 +3,7 @@ import React from 'react';
 export default function Spinner() {
   return (
     <div className='spinner'>
-      <div></div>
-      <div></div>
-      <div></div>
+      {[1, 2, 3].map(i => <div></div>)}
     </div>
   );
 }
